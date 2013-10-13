04b1ed2d64205e47d45ea38c2b8518651d7b3f3c
Owen Garland <Garland.owen@gmail.com>
2013-10-13 12:59:23 +0100

    Added comments to server.js. Mainly to test git SSH keys

--- server.js
+++ server.js
@@ -3,30 +3,39 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+//Serve the public directory
 app.configure(function () {
     app.use( "/", express.static(__dirname + '/public'));
 });
 
+//On port 80
 app.listen(80); 
 
+//Initialise the array of clients
 var clients = [];
 
+//Sockets
 io.sockets.on('connection', function (socket) {
+
+  //Add new client to array
   clients.push(socket);
   console.log("Client connected");
 
+  //Return codes to client on submission
   socket.on('domainSubmit', function(data) {
     getStatusCode(data.domainName, function(statusCode, errorCode) {
         socket.emit('result', {'errorCode': errorCode, 'status': statusCode});
     });
   });
 
+  //Remove client from array on disconnect
   socket.on('disconnect', function() {
     clients.splice(clients.indexOf(socket), 1);
     console.log("Client disconnected");
   });
 });
 
+//Perform the http.get request
 function getStatusCode(domain, callback) {
   var target = "http://" + domain; //Need to test for http later
   http.get(target, function(res) {

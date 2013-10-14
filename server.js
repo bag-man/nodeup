501be14f7f003fbcc649542436db4cff33f06244
Owen Garland <Garland.owen@gmail.com>
2013-10-14 23:16:08 +0100

    Changed code layout

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //Require modules
-
-var express = require('express'); //I am requiring this twice which I don't like. I can't see a way round :/
+var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
@@ -11,12 +10,14 @@ var http = require('follow-redirects').http;
 server.listen(80);
 
 //Serve the public directory
-app.configure(function () {
+app.configure(function ()
+{
   app.use(express.static(__dirname + '/public'));
 });
 
-//Accept an argument 
-app.get('/:domain', function(req, res){
+//Accept an argument
+app.get('/:domain', function(req, res)
+{
   res.sendfile(__dirname + '/public/index.html');
 });
 
@@ -25,32 +26,39 @@ var clients = [];
 var handler;
 
 //Sockets connect and disconnect
-io.sockets.on('connection', function (socket) {
-
+io.sockets.on('connection', function (socket)
+{
   //Add new client to array
   clients.push(socket);
   console.log("Client connected");
-
+  
   //Remove client from array on disconnect
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function()
+  {
     clients.splice(clients.indexOf(socket), 1);
     clearInterval(handler);
     console.log("Client disconnected");
   });
-
+  
   //Return codes to client on submission and keep refreshing
-  socket.on('domainSubmit', function(data) {
-    if(clients.indexOf(socket)) {
-	  clearInterval(handler);
-	}
-	var check = function() {
-      getStatusCode(data.domainName, function(statusCode, errorCode) {
-		if(statusCode == null) {
-		  var up = false;
-		} else {
-		  var up = upFinder(statusCode);
-		}
-		socket.emit('result', {'up': up });
+  socket.on('domainSubmit', function(data)
+  {
+    if(clients.indexOf(socket))
+    {
+      clearInterval(handler)
+    }
+    var check = function()
+    {
+      getStatusCode(data.domainName, function(statusCode, errorCode)
+      {
+        if(statusCode == null)
+        {
+ 	  var up = false;
+        } else
+        {
+  	  var up = upFinder(statusCode);
+        }
+        socket.emit('result', {'up': up });
       });
     };
     check();
@@ -59,18 +67,23 @@ io.sockets.on('connection', function (socket) {
 });
 
 //Perform the http.get request
-function getStatusCode(domain, callback) {
+function getStatusCode(domain, callback)
+{
   var target = "http://" + domain; //Need to test for http later
-  http.get(target, function(res) {
-      callback(res.statusCode, null);
-  }).on('error', function(e) { 
-      callback(null, e);
+  http.get(target, function(res)
+  {
+    callback(res.statusCode, null);
+  }).on('error', function(e)
+  {
+    callback(null, e);
   });
 }
 
 //Test the status code
-function upFinder(code) {
-  if(code >= 200 && code <= 203) {
+function upFinder(code)
+{
+  if(code >= 200 && code <= 203)
+  {
     return true;
   }
   return false;

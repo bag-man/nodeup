4079f06821d870ffae3ebe30b0f5db781328a9dc
Ilia Choly <ilia.choly@gmail.com>
2013-10-17 01:06:15 -0400

    refactor

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 //Require modules
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-io.set('log level', 1); // reduce logging
-var http = require('follow-redirects').http;
+var express 				= require('express');
+		http 						= require('http'),
+		socketIo      	= require('socket.io'),
+		followRedirects = require('follow-redirects');
+
+var app 	 = express(),
+		server = http.createServer(app),
+		io 		 = socketIo.listen(server);
+		
+// reduce logging
+io.set('log level', 1); 
 
 //Start server
 server.listen(80);
 
 //Serve the public directory
-app.configure(function ()
+app.configure(function()
 {
   app.use(express.static(__dirname + '/public'));
 });
@@ -21,64 +26,46 @@ app.get('/:domain', function(req, res)
   res.sendfile(__dirname + '/public/index.html');
 });
 
-//Create clients array
-var clients = [];
-
 //Sockets connect and disconnect
 io.sockets.on('connection', function (socket)
 {
-  //Create client var
-  var client;
+  //Interval for updating domain status
+  var interval = null;
 
   //Add new client to array
-  socket.emit('id', {'id': socket.id});
-  clients.push(socket);
-  console.log(socket.id + " connected.");
+  socket.emit('id', { 'id' : socket.id });
+  console.log(socket.id, "connected.");
   
   //Remove client from array on disconnect
-  socket.on('disconnect', function()
-  {
-    clearInterval(clients[client].handler);
-    clients.splice(clients.indexOf(socket), 1);
-    console.log(socket.id + " disconnected.");
+  socket.on('disconnect', function () {
+  	if (handler !== null) {
+  		clearInterval(handler);	
+  	} 
+    console.log(socket.id, "disconnected.");
   });
   
   //Return codes to client on submission and keep refreshing
-  socket.on('domainSubmit', function(data)
+  socket.on('domainSubmit', function(data) 
   {
-    for(i=0; i<clients.length; i++)
-    {
-      if(clients[i].id == data.id)
-      {
-        client = i; 
-
-        if(clients[i].handler)
-	{
-	  console.log("Client " + clients[i].id + " already has a handler. Killing old one.");
-	  clearInterval(clients[i].handler);
-	}
-      }
-    }
-
     //Function for looping
     var check = function()
     {
-      console.log("Client " + clients[client].id + " is running " + data.domainName);
+      console.log("Client", socket.id, "is running", data.domainName);
       getStatusCode(data.domainName, function(statusCode, errorCode)
       {
-	if(statusCode == null)
-	{
-	  var up = false;
-	} else
-	{
-	  var up = upFinder(statusCode);
-	}
-	socket.emit('result', {'up': up });
+				var up = (statusCode == null) ? upFinder(statusCode) : false;
+				socket.emit('result', { 'up': up });
       });
     };
+    
+    //Clear any existing intervals
+		if (handler !== null) {
+	  	console.log("Client", socket.id, "already has a handler. Killing old one.");
+	  	clearInterval(handler);
+		}
 
     check();
-    clients[client].handler = setInterval(check, 5000);
+    handler = setInterval(check, 5000);
   });
 });
 
@@ -86,10 +73,10 @@ io.sockets.on('connection', function (socket)
 function getStatusCode(domain, callback)
 {
   var target = "http://" + domain; //Need to test for http later
-  http.get(target, function(res)
+  followRedirects.http.get(target, function(res)
   {
     callback(res.statusCode, null);
-  }).on('error', function(e)
+  }).on('error', function(e) 
   {
     callback(null, e);
   });
@@ -99,9 +86,5 @@ function getStatusCode(domain, callback)
 //Test the status code
 function upFinder(code)
 {
-  if(code >= 200 && code <= 203)
-  {
-    return true;
-  }
-  return false;
+  return code >= 200 && code <= 203;
 }

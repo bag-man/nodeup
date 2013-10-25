419cda5965782713683008a40f8521f0e81a368a
Jamie Knight <jamie@theknights.org.uk>
2013-10-25 19:59:31 +0100

    Fixed a few niggles. Still working on getting it actually working.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
      server = require('http').createServer(app),
          io = require('socket.io').listen(server),
        http = require('follow-redirects').http,
-    monitor = require('monitor.js');
+    monitor = require('./monitor.js');
 
 //Array of objects for each domain
 var domains = [];
@@ -45,36 +45,39 @@ io.sockets.on('connection', function (socket)
   //Return codes to client on submission and keep refreshing
   socket.on('domainSubmit', function(data)
   {
+    var domains   = [ {domain: data.domain, id: socket.id} ]; // "Temporary"
     for(i = 0; i < domains.length; i++)
     {
       if(!domains[i].domain) 
       {
-	domains.push(new Monitor(data.domain, data.id));
-	var result = domains[i++].checkDomain();
+	      domains.push(new Monitor(data.domain, data.id));
+        var result = domains[i++].checkDomain();
       } else {
         domains[i].addClient(data.id);
       }
 
-    //Function for looping
-    var check = function()
-    {
-      getStatusCode(data.domainName, function(statusCode, errorCode)
+      //Function for looping
+      var check = function()
       {
-	if(statusCode == null)
-	{
-	  var up = false;
-	} else
-	{
-	  var up = upFinder(statusCode);
-	}
-	socket.emit('result', {'up': up, 'domain': data.domainName});
-      });
-    };
 
-    check();
-    if(typeof client !== 'undefined') //This doesn't work quite right
-    {
-      clients[client].handler = setInterval(check, 5000);
+        getStatusCode(data.domainName, function(statusCode, errorCode)
+        {
+          if(statusCode == null)
+          {
+            var up = false;
+          } else
+          {
+            var up = upFinder(statusCode);
+          }
+          socket.emit('result', {'up': up, 'domain': data.domainName});
+        });
+      };
+
+      check();
+      if(typeof client !== 'undefined') //This doesn't work quite right
+      {
+        clients[client].handler = setInterval(check, 5000);
+      }
     }
   });
 });

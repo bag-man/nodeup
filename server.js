b70e3d54b4238fc2d84e730e15cd00ed93b24e5b
Owen Garland <Garland.owen@gmail.com>
2013-10-25 20:32:52 +0100

    See server.js:52

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ io.sockets.on('connection', function (socket)
     {
       domains[data.domain] = new monitor(data.domain, socket.id);
 
-      setInterval(domains[data.domain].checkDomain,5000);
+      domains.[data.domain].handler = setInterval(domains[data.domain].checkDomain,5000);
       //If this is true then 
       //socket.emit('result', {'up': up, 'domain': data.domainName});
       //to all clients in the domain object

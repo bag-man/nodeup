c350185d6b390de0ccbcfb05d09d886194f667d5
Owen Garland <Garland.owen@gmail.com>
2013-10-25 20:00:52 +0100

    Getting there, see server.js:53

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var express = require('express'),
      server = require('http').createServer(app),
          io = require('socket.io').listen(server),
        http = require('follow-redirects').http,
-    monitor = require('monitor.js');
+    monitor = require('./monitor.js');
 
 //Array of objects for each domain
-var domains = [];
+var domains = {};
 
 //Reduce logging
 io.set('log level', 1); 
@@ -45,60 +45,17 @@ io.sockets.on('connection', function (socket)
   //Return codes to client on submission and keep refreshing
   socket.on('domainSubmit', function(data)
   {
-    for(i = 0; i < domains.length; i++)
+    if(!domains[data.domain])
     {
-      if(!domains[i].domain) 
-      {
-	domains.push(new Monitor(data.domain, data.id));
-	var result = domains[i++].checkDomain();
-      } else {
-        domains[i].addClient(data.id);
-      }
+      domains[data.domain] = new Monitor(data.domain, data.id);
 
-    //Function for looping
-    var check = function()
-    {
-      getStatusCode(data.domainName, function(statusCode, errorCode)
-      {
-	if(statusCode == null)
-	{
-	  var up = false;
-	} else
-	{
-	  var up = upFinder(statusCode);
-	}
-	socket.emit('result', {'up': up, 'domain': data.domainName});
-      });
-    };
-
-    check();
-    if(typeof client !== 'undefined') //This doesn't work quite right
-    {
-      clients[client].handler = setInterval(check, 5000);
+      setInterval(domains[data.domain].checkDomain(),5000);
+      //If this is true then 
+      io.clients[data.id].emit('result', {'up': up, 'domain': data.domainName});
+      //to all clients in the domain object
+    } else  {
+      domains[data.domain].addClient(data.id);
     }
+
   });
 });
-
-//Perform the http.get request
-function getStatusCode(domain, callback)
-{
-  var target = "http://" + domain; //Need to test for http later
-  http.get(target, function(res)
-  {
-    callback(res.statusCode, null);
-  }).on('error', function(e)
-  {
-    callback(null, e);
-  });
-}
-
-
-//Test the status code
-function upFinder(code)
-{
-  if(code >= 200 && code <= 203)
-  {
-    return true;
-  }
-  return false;
-}

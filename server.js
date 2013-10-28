1dd0789b48e8bbd977593a10f91c72d6acc7dfe5
Jamie Knight <jamie@theknights.org.uk>
2013-10-28 20:40:12 +0000

    Nothing to see here, move along.

--- server.js
+++ server.js
@@ -31,12 +31,13 @@ io.sockets.on('connection', function (socket) {
     //On domainSubmit the client disconnects.
     for(var i in domains) {
       domains[i].removeClient(socket.id); 
-      console.log(socket.id + " removed from " + domains[i]); 
+      console.log(socket.id + " removed from " + domains[i].domain);
     }
     if(!domains[data.domain]) {
       domains[data.domain] = new Monitor(data.domain);
       domains[data.domain].start();
     }
+    console.log(socket.id + " adding to " + domains[data.domain].domain);
     domains[data.domain].addClient(socket.id, function(up) {
       socket.emit('result', {'up': up, 'domain': data.domain});
     });
